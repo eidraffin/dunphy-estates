9abbc7543c8b8162b8e05c35ad3504acb42a0279
Eidraffin Islam <100798293+eidraffin@users.noreply.github.com>
2024-10-04 05:32:39 +0600

    Update PrivateRoute.jsx

--- components/auth/PrivateRoute.jsx
+++ components/auth/PrivateRoute.jsx
@@ -1,15 +1,3 @@
-/*import { Outlet, Navigate } from "react-router-dom";
-import { useAuthStatus } from "../hooks/useAuthStatus";
-import Spinner from "./Spinner";
-
-export default function PrivateRoute() {
-  const { loggedIn, checkingStatus } = useAuthStatus();
-  if (checkingStatus) {
-    return <Spinner />;
-  }
-  return loggedIn ? <Outlet /> : <Navigate to="/auth/login" />;
-}*/
-
 "use client"; 
 
 import { useRouter } from "next/navigation";
